90783e8a7c01008f27607e625fcaebe2f4e98f26
Your Name <your.email@example.com>
2026-06-04 12:22:45 +0300

    first commit

--- resources/views/front/partials/home/gallery.blade.php
+++ resources/views/front/partials/home/gallery.blade.php
@@ -5,7 +5,7 @@
             <p>{{ __('A glimpse into our training environment, graduation ceremonies, and daily student life at Al-Ertiqaa.') }}</p>
         </div>
         <div class="gallery-grid">
-            <div class="gallery-item gallery-item-1"><img loading="lazy" src="{{ $lms3s('2.png') }}" alt="معهد الارتقاء" /><div class="overlay"></div></div>
+            <div class="gallery-item gallery-item-1"><img loading="lazy" src="{{ $lms3s('1.png') }}" alt="معهد الارتقاء" /><div class="overlay"></div></div>
             <div class="gallery-item gallery-item-2"><img loading="lazy" src="{{ $lms3s('4.png') }}" alt="قاعة التدريب" /><div class="overlay"></div></div>
             <div class="gallery-item gallery-item-3"><img loading="lazy" src="{{ $lms3s('5.png') }}" alt="المتدربون" /><div class="overlay"></div></div>
             <div class="gallery-item gallery-item-4"><img loading="lazy" src="{{ $lms3s('9.png') }}" alt="التخرج" /><div class="overlay"></div></div>
