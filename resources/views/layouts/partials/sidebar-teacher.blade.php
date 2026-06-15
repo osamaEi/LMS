58a77a9350432b7cd67dd6309cf27e99aed26346
Your Name <your.email@example.com>
2026-06-15 16:24:59 +0300

    pp

--- resources/views/layouts/partials/sidebar-teacher.blade.php
+++ resources/views/layouts/partials/sidebar-teacher.blade.php
@@ -69,7 +69,9 @@ $_courseTypeConfig = [
 <li>
     <a href="{{ route('teacher.my-courses.index') }}"
        class="menu-item group relative flex items-center gap-3 rounded-lg px-4 py-3 font-medium {{ request()->routeIs('teacher.my-courses.*') ? 'menu-item-active' : 'menu-item-inactive' }}">
-      
+        <svg class="fill-current" style="fill:none;stroke:currentColor;stroke-width:2;" width="20" height="20" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"/>
+        </svg>
         <span>الدورات</span>
     </a>
 </li>
